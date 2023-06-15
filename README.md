de6d68b114eccf0ed85f39cd9185ef09452919ae
GolamMuid <golammuid@gmail.com>
2023-06-15 23:37:45 +0600

    added error handles

--- README.md
+++
@@ -1,1 +0,0 @@
-"# Basic-express-backend" 
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,46 @@
+import { Server } from 'http'
 import mongoose from 'mongoose'
-import config from './config/index'
 import app from './app'
+import config from './config/index'
+import { errorlogger, logger } from './shared/logger'
+
+process.on('uncaughtException', error => {
+  errorlogger.error(error)
+  process.exit(1)
+})
+
+let server: Server
 
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log(`Databse connected successfully`)
+    logger.info(`Databse connected successfully`)
 
-    app.listen(config.port, () => {
-      console.log(`Application listening on port ${config.port}`)
+    server = app.listen(config.port, () => {
+      logger.info(`Application listening on port ${config.port}`)
     })
-  } catch (error) {
-    console.log(`Failed to connect to Mongo`)
+  } catch (err) {
+    logger.error(`Failed to connect to Mongo`)
   }
 }
 
+process.on('unhandledRejection', error => {
+  console.log('Unhandled rejection is detected, server is closing')
+  if (server) {
+    server.close(() => {
+      errorlogger.error(error)
+      process.exit(1)
+    })
+  } else {
+    process.exit(1)
+  }
+})
+
 bootstrap()
+
+process.on('SIGTERM', () => {
+  logger.info(`SIGTERM is received`)
+  if (server) {
+    server.close()
+  }
+})
